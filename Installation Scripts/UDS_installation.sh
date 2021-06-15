1d1ab59d63279cb1c1043c574a9db7d5f933dc61
Vishwajit Dandage <83405861+vishwajitdandage-psl@users.noreply.github.com>
2021-06-15 18:21:37 +0530

    Update and rename BAS_installation.sh to UDS_installation.sh

--- Installation Scripts/UDS_installation.sh
+++ Installation Scripts/UDS_installation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-## This Script installs BAS operator using default values. 
-source bas-script-functions.bash
+## This Script installs UDS operator using default values. 
+source uds-script-functions.bash
 source cr.properties
 
 requiredVersion="^.*4\.([0-9]{3,}|[3-9]?)?(\.[0-9]+.*)*$"
 requiredServerVersion="^.*1\.([0-9]{16,}|[3-9]?)?(\.[0-9]+)*$"
 
 
-logFile="bas-installation.log"
+logFile="uds-installation.log"
 touch "${logFile}"
 
 validatePropertiesfile
@@ -18,7 +18,7 @@ checkOCClientVersion
 
 status=$(oc whoami 2>&1)
 if [[ $? -gt 0 ]]; then
-    echoRed "Login to OpenShift to continue BAS Operator installation."
+    echoRed "Login to OpenShift to continue UDS Operator installation."
         exit 1;
 fi
 
@@ -27,11 +27,11 @@ createProject
 
 displayStepHeader 2 "Create an OperatorGroup object YAML file"
 
-cat <<EOF>bas-og.yaml
+cat <<EOF>uds-og.yaml
 apiVersion: operators.coreos.com/v1
 kind: OperatorGroup
 metadata:
-  name: bas-operator-group
+  name: uds-operator-group
   namespace: "${projectName}" 
 spec: 
   targetNamespaces:
@@ -40,40 +40,40 @@ EOF
 
 displayStepHeader 3 "Create the OperatorGroup object"
 
-oc create -f bas-og.yaml &>>"${logFile}"
+oc create -f uds-og.yaml &>>"${logFile}"
 
 displayStepHeader 4 "Create a Subscription object YAML file to subscribe a Namespace"
 
-cat <<EOF>bas-subscription.yaml
+cat <<EOF>uds-subscription.yaml
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
-  name: behavior-analytics-services-operator-certified
+  name: user-data-services-operator-certified
   namespace: "${projectName}"
 spec:
   channel: alpha
   installPlanApproval: Automatic
-  name: behavior-analytics-services-operator-certified
+  name: user-data-services-operator-certified
   source: certified-operators
   sourceNamespace: openshift-marketplace
-  startingCSV: behavior-analytics-services-operator.v1.0.0
+  startingCSV: user-data-services-operator.v1.0.0
 EOF
 
 
 displayStepHeader 5 "Create Subscription object"
 
-oc create -f bas-subscription.yaml &>>"${logFile}"
+oc create -f uds-subscription.yaml &>>"${logFile}"
 
 
 displayStepHeader 6 "Verify the Operator installation"
-#There should be behavior-analytics-services-operator.v1.0.0.
+#There should be user-data-services-operator.v1.0.0.
 
 check_for_csv_success=$(checkClusterServiceVersionSucceeded 2>&1)
 
 if [[ "${check_for_csv_success}" == "Succeeded" ]]; then
-	echoGreen "Behavior Analytics Services Operator installed"
+	echoGreen "User Data Services Operator installed"
 else
-    echoRed "Behavior Analytics Services Operator installation failed."
+    echoRed "User Data Services Operator installation failed."
 	exit 1;
 fi
 
@@ -81,30 +81,34 @@ displayStepHeader 7 "Create a secret named database-credentials for PostgreSQL D
 
 oc create secret generic database-credentials --from-literal=db_username=${dbuser} --from-literal=db_password=${dbpassword} -n "${projectName}" &>>"${logFile}"
 
-oc create secret generic grafana-credentials --from-literal=grafana_username=${grafanauser} --from-literal=grafana_password=${grafanapassword} -n "${projectName}" &>>"${logFile}"
+oc create secret generic consent-database-credentials --from-literal=consent_db_username=${consent_db_username} --from-literal=consent_db_password=${consent_db_password} -n "${projectName}" &>>"${logFile}"
+
+oc create secret generic consent-ui-credentials --from-literal=consent_username=${consent_ui_username} --from-literal=consent_password=${consent_ui_password} -n "${projectName}" &>>"${logFile}"
+
 
 
 displayStepHeader 8 "Create the yaml for AnalyticsProxy instance."
 
 
 cat <<EOF>analytics-proxy.yaml
-apiVersion: bas.ibm.com/v1
+apiVersion: uds.ibm.com/v1
 kind: AnalyticsProxy
 metadata:
   name: analyticsproxydeployment
 spec:
-  allowed_domains: "*"
+ allowed_domains: "*"
   db_archive:
     frequency: '@monthly'
     retention_age: 6
     persistent_storage:
       storage_class: "${storageClassArchive}"
       storage_size: "${storageSizeArchive}"
-  airgapped:
+  airgappeddeployment:
     enabled: "${airgappedEnabled}"
     backup_deletion_frequency: '@daily'
     backup_retention_period: 7
   event_scheduler_frequency: "${eventSchedulerFrequency}"
+  consent_scheduler_frequency: "${consent_scheduler_frequency}"
   ibmproxyurl: "${ibmproxyurl}"
   image_pull_secret: "${imagePullSecret}"
   postgres:
@@ -115,6 +119,8 @@ spec:
     storage_size: "${storageSizeKafka}"
     zookeeper_storage_class: "${storageClassZookeeper}"
     zookeeper_storage_size: "${storageSizeZookeeper}"
+  prometheus_scheduler_frequency: "${prometheusSchedulerFrequency}"
+  prometheus_metrics: []
   env_type: "${envType}"
 EOF
 
@@ -136,12 +142,12 @@ fi
 displayStepHeader 10 "Generate an API Key to use it for authentication"
 
 cat <<EOF>api-key.yaml
-apiVersion: bas.ibm.com/v1
+apiVersion: uds.ibm.com/v1
 kind: GenerateKey
 metadata:
-  name: bas-api-key
+  name: uds-api-key
 spec:
-  image_pull_secret: bas-images-pull-secret
+  image_pull_secret: uds-images-pull-secret
 EOF
 
 
@@ -152,15 +158,14 @@ oc create -f api-key.yaml
 check_for_key=$(getGenerateAPIKey)
 
 #Get the URLS
-bas_endpoint_url=https://$(oc get routes bas-endpoint -n "${projectName}" |awk 'NR==2 {print $2}')
-grafana_dashboard_url=https://$(oc get routes grafana-route -n "${projectName}" |awk 'NR==2 {print $2}')
-
+uds_endpoint_url=https://$(oc get routes uds-endpoint -n "${projectName}" |awk 'NR==2 {print $2}')
+consent_endpoint_url=https://$(oc get routes consent-endpoint -n "${projectName}" |awk 'NR==2 {print $2}')
 
 displayStepHeader 12 "Get the API key value and the URLs"
 echo "===========API KEY=============="
 echoYellow $check_for_key
-echo "===========BAS Endpoint URL=============="
-echoYellow $bas_endpoint_url
-echo "===========Grafana URL=============="
-echoYellow $grafana_dashboard_url
+echo "===========UDS Endpoint URL=============="
+echoYellow $uds_endpoint_url
+echo "===========Consent URL=============="
+echoYellow $consent_endpoint_url
 
