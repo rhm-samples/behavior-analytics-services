a0f3d18ea8cbc6df4501aa069dd59430bf454e79
Vishwajit Dandage <83405861+vishwajitdandage-psl@users.noreply.github.com>
2022-01-05 17:27:14 +0530

    updated to install BAS113

--- Installation Scripts/bas-script-functions.bash
+++ Installation Scripts/bas-script-functions.bash
@@ -105,8 +105,8 @@ function checkOpenshiftVersion() {
   currentOpenshiftVersion="$(oc version -o json | jq .openshiftVersion)"
   echo OCP version is "$currentOpenshiftVersion"
   if [[ $currentOpenshiftVersion =~ $ocpVersion ]]; then
-    echo "install BAS 1.1.2"
-    basVersion=-certified.v1.1.2
+    echo "install BAS 1.1.3"
+    basVersion=-certified.v1.1.3
   elif [[ $currentOpenshiftVersion =~ $ocpVersion45 ]]; then
     echo "install BAS 1.0.0"
     basVersion=.v1.0.0
